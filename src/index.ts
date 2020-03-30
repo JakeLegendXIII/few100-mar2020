496d27964574b7a2b1db46d6979e173e55047e74
Jake Kirkbride <jakelegendxiii@gmail.com>
2020-03-30 19:55:37 +0000

    final changes for first file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,41 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript! Jake was here!');
+
+interface ShoppingItem {
+    description: string;
+}
+
+const items: ShoppingItem[] = [
+    { description: 'Toilet Paper' },
+    { description: 'Hand Sanitizer' },
+    { description: 'Beer' }
+];
+
+const theList = document.getElementById('the-list') as HTMLUListElement;
+const theButton = document.getElementById('addButton') as HTMLButtonElement;
+const itemToAdd = document.getElementById('itemToAdd') as HTMLInputElement;
+drawList();
+
+theButton.addEventListener('click', addTheItem);
+
+function addTheItem() {
+    const newItem: ShoppingItem = {
+        description: itemToAdd.value
+    };
+    items.unshift(newItem);
+    drawList();
+}
+
+function drawList() {
+    items.forEach(item => {
+        const newElem = document.createElement('li');
+        newElem.classList.add('alert');
+        newElem.classList.add('alert-info');
+        const newSpan = document.createElement('span');
+        newSpan.innerText = item.description;
+        newElem.appendChild(newSpan);
+        theList.appendChild(newElem);
+    });
+
+}
 
